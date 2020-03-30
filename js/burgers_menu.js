66f5b2950685898eaac1d86cc788640f81359efb
Anna Demisheva <demisheva.anna@gmail.com>
2020-03-30 16:24:02 +0200

    made adaptive products.html

--- js/burgers_menu.js
+++ js/burgers_menu.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
     let burgerBtn = document.getElementById("burger-btn");
     let menuBurger = document.getElementById("menu-burger");
+    let burgerFilterBtn = document.getElementById("burger-filter-btn");
+    let filterBurger = document.getElementById("filter-burger");
 
     function showBurgerMenu() {
         menuBurger.classList.toggle("burger-menu-show");
     };
     burgerBtn.onclick = showBurgerMenu;
+
+    function showBurgerFilter() {
+        filterBurger.classList.toggle("burger-filter-show");
+    };
+    burgerFilterBtn.onclick = showBurgerFilter;
+
+
 })
